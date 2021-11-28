58f5e5de7739c001120395d418464a5747442cb7
Hakan Saplakoglu <hakansaplakog@gmail.com>
2021-11-28 08:20:13 +0000

    switch to rotation method instead of tp on each chunk

--- pregenerate.py
+++ pregenerate.py
@@ -3,11 +3,12 @@ import math
 import time
 import pexpect
 import os
+import datetime
 
 def main():
     # Handle cli arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--delay", help="Time between delays", type=float, default=0.2, required=False)
+    parser.add_argument("-d", "--delay", help="Time between delays", type=float, default=0.1, required=False)
     parser.add_argument("-m", "--memory", help="Memory to allocate to JVM", type=int, default=2048, required=False)
     parser.add_argument("player", help="Player name to expect on server")
     parser.add_argument("server", help="Path to minecraft server.jar")
@@ -25,13 +26,20 @@ def main():
         print("JAR path does not exist.")
         exit(1)
 
+    if PARAM_RADIUS % 1024 != 0:
+        print("Please use a radius which is a multiple of 1024.")
+        exit(1)
+
     startCoord = -1 * PARAM_RADIUS
     endCoord = PARAM_RADIUS
 
-    totalChunks = math.floor(PARAM_RADIUS**2 * 4 / 16)
+    PARAM_GAP = 256
+
+    totalChunks = math.floor(PARAM_RADIUS**2 * 4)
     print("Required generation of %i chunks" % totalChunks)
 
-    estimatedTime = totalChunks * PARAM_DELAY / 3600
+    totalLogicalChunks = math.floor((PARAM_RADIUS / PARAM_GAP)**2 * 4)
+    estimatedTime = totalLogicalChunks * PARAM_DELAY * 4 / 3600
     print("Estimated to take at least %.2f hours" % estimatedTime)
     input("Press any key to start the server and begin pregeneration...")
 
@@ -45,25 +53,54 @@ def main():
     print("Waiting for %s to join server..." % PARAM_PLAYER)
     child.expect("%s joined the game" % PARAM_PLAYER, timeout=300)  # wait for player to join
     child.sendline("gamemode creative %s" % PARAM_PLAYER)  # set gamemode to creative
+    child.sendline("gamerule doDaylightCycle false")  # set gamemode to creative
+    child.sendline("gamemode doWeatherCycle false")  # set gamemode to creative
+    child.sendline("gamemode doMobSpawning false")  # set gamemode to creative
     
     print("Starting chunk pregeneration...")
 
+    begin_time = datetime.datetime.now()
+
     # start pregeneration
-    chunkRange = range(startCoord, endCoord, 16)
+    chunkRange = range(startCoord, endCoord, PARAM_GAP)
     count = 0
     lastFloor = -1
+    opposite = 1
+    lastPerc = -1
     for x_coord in chunkRange:
         for z_coord in chunkRange:
-            child.sendline('tp %s %i 255 %i' % (PARAM_PLAYER, x_coord, z_coord))
+            z_coord = z_coord * opposite
+
+            child.sendline('tp %s %i 255 %i 0 45' % (PARAM_PLAYER, x_coord, z_coord))
             child.expect('Teleported %s' % PARAM_PLAYER)
-
-            count = count + 1
-            perc = (float(count) / float(totalChunks)) * 100
-            print("%.2f Percent Done" % perc)
-
+            time.sleep(PARAM_DELAY)
+            child.sendline('tp %s %i 255 %i -90 45' % (PARAM_PLAYER, x_coord, z_coord))
+            child.expect('Teleported %s' % PARAM_PLAYER)
+            time.sleep(PARAM_DELAY)
+            child.sendline('tp %s %i 255 %i -180 45' % (PARAM_PLAYER, x_coord, z_coord))
+            child.expect('Teleported %s' % PARAM_PLAYER)
+            time.sleep(PARAM_DELAY)
+            child.sendline('tp %s %i 255 %i -90 45' % (PARAM_PLAYER, x_coord, z_coord))
+            child.expect('Teleported %s' % PARAM_PLAYER)
             time.sleep(PARAM_DELAY)
 
-    print("Chunk Pregeneration Done, stopping server")
+            count = count + 1
+            perc = round((float(count) / float(totalLogicalChunks)) * 100, 2)
+            if perc != lastPerc:
+                print("%.2f Percent Done" % perc)
+                lastPerc = perc
+
+            #time.sleep(PARAM_DELAY)
+        opposite = opposite * -1
+
+    elapsed = datetime.datetime.now() - begin_time
+    print("Chunk pregeneration took " + str(elapsed))
+
+    child.sendline("gamerule doDaylightCycle true")  # set gamemode to creative
+    child.sendline("gamemode doWeatherCycle true")  # set gamemode to creative
+    child.sendline("gamemode doMobSpawning true")  # set gamemode to creative
+
+    input("Press any key to close the server...")
 
 if __name__ == '__main__':
     main()
